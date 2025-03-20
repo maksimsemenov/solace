d2e105e2defae539592633de02e0946567b50625
Maksim Semenov <maksa.semenov@gmail.com>
2025-03-20 08:45:05 -0400

    feat: improve search and sorting

--- src/modules/advocates/useAdvocates.ts
+++ src/modules/advocates/useAdvocates.ts
@@ -24,15 +24,29 @@ export const useAdvocates = ({ query }: IUseAdvocatesProps): IUseAdvocates => {
 	const filteredAdvocates = useMemo(() => {
 		if (!query) return advocates
 
-		return advocates.filter((advocate) => {
-			return (
-				advocate.firstName.includes(query) ||
-				advocate.lastName.includes(query) ||
-				advocate.city.includes(query) ||
-				advocate.degree.includes(query) ||
-				advocate.specialties.includes(query) ||
-				advocate.yearsOfExperience.toString().includes(query)
+		const queryParts = query.toLowerCase().split(/[ -\W]/)
+		const result: IAdvocate[] = []
+
+		for (const advocate of advocates) {
+			const match = queryParts.every(
+				(part) =>
+					advocate.firstName.toLowerCase().includes(part) ||
+					advocate.lastName.toLowerCase().includes(part) ||
+					advocate.city.toLowerCase().includes(part) ||
+					advocate.degree.toLowerCase().includes(part) ||
+					advocate.specialties.some((s) => s.toLowerCase().includes(part)) ||
+					advocate.phoneNumber.toString().includes(part)
 			)
+			if (match) {
+				result.push(advocate)
+			}
+		}
+
+		return result.sort((a, b) => {
+			const years = b.yearsOfExperience - a.yearsOfExperience
+			if (years) return years
+
+			return a.firstName.localeCompare(b.firstName)
 		})
 	}, [advocates, query])
 
